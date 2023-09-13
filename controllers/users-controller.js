bf8710d285951462ff42325d1429b9515372f46a
tkulagina <tkulagina1974@gmail.com>
2023-09-13 11:35:46 +0300

    fix logout

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -70,9 +70,7 @@ const logout = async (req, res) => {
     const {_id} = req.user;
     await User.findByIdAndUpdate (_id, {token: ""});
 
-    res.json ({
-        
-    })
+    res.status(204).json();
 }
 
 module.exports = {
