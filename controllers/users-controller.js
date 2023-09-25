c577cfcc7a799851eeee447ea9607b35eed115b6
tkulagina <tkulagina1974@gmail.com>
2023-09-25 12:18:22 +0300

    update

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -59,7 +59,7 @@ const verifyEmail = async (req, res) => {
     })
 }
 
-/*const resendVerifyEmail = async (req, res) => {
+const resendVerifyEmail = async (req, res) => {
     const { email } = req.body;
     const user = await User.findOne({email});
     if (!user) {
@@ -81,31 +81,8 @@ const verifyEmail = async (req, res) => {
     res.json ({
         "message": "Verification email sent"
     })
-}*/
+}
 
-const resendVerifyEmail = async (req, res) => {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw HttpError(404, "Email not found");
-    }
-  
-    if (user.verify) {
-      throw HttpError(400, "Email alredy verify")
-    }
-  
-    const verifyEmail = {
-      to: email,
-      subject: "Verify email",
-      html: `<a target="_blanc" href="${BASE_URL}/api/auth/verify/${user.verificationToken}">Click verify email</a>`,
-    };
-  
-    await sendEmail(verifyEmail);
-  
-    res.json({
-      message: "Email resend success"
-    });
-  };
 
 const login = async (req, res) => {
     const {email, password} = req.body;
